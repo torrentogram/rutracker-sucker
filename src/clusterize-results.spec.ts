4d7cb38af5450bab9a0e05db02cbe87430a4f01c
no <no>
2019-12-08 15:30:46 +0200

    more deterministic tests

--- src/clusterize-results.spec.ts
+++ src/clusterize-results.spec.ts
@@ -10,15 +10,15 @@ describe('clusterizeResults', () => {
                 forumName: '',
                 size: 0,
                 topicId: 0,
-                topicUrl: null
+                topicUrl: null,
             },
             {
                 title: 'big',
-                seeds: 23,
+                seeds: 170,
                 forumName: '',
                 size: 0,
                 topicId: 0,
-                topicUrl: null
+                topicUrl: null,
             },
             {
                 title: 'tiny',
@@ -26,15 +26,15 @@ describe('clusterizeResults', () => {
                 forumName: '',
                 size: 0,
                 topicId: 0,
-                topicUrl: null
+                topicUrl: null,
             },
             {
                 title: 'huge',
-                seeds: 54,
+                seeds: 200,
                 forumName: '',
                 size: 0,
                 topicId: 0,
-                topicUrl: null
+                topicUrl: null,
             },
             {
                 title: 'not big',
@@ -42,7 +42,7 @@ describe('clusterizeResults', () => {
                 forumName: '',
                 size: 0,
                 topicId: 0,
-                topicUrl: null
+                topicUrl: null,
             },
             {
                 title: 'average',
@@ -50,64 +50,66 @@ describe('clusterizeResults', () => {
                 forumName: '',
                 size: 0,
                 topicId: 0,
-                topicUrl: null
-            }
+                topicUrl: null,
+            },
         ];
 
-        const clusterBig = [
-            {
-                title: 'huge',
-                seeds: 54,
-                forumName: '',
-                size: 0,
-                topicId: 0,
-                topicUrl: null
-            },
-            {
-                title: 'big',
-                seeds: 23,
-                forumName: '',
-                size: 0,
-                topicId: 0,
-                topicUrl: null
-            }
-        ];
-
-        const clusterSmall = [
-            {
-                title: 'average',
-                seeds: 12,
-                forumName: '',
-                size: 0,
-                topicId: 0,
-                topicUrl: null
-            },
-            {
-                title: 'not big',
-                seeds: 5,
-                forumName: '',
-                size: 0,
-                topicId: 0,
-                topicUrl: null
-            },
-            {
-                title: 'small',
-                seeds: 2,
-                forumName: '',
-                size: 0,
-                topicId: 0,
-                topicUrl: null
-            },
-            {
-                title: 'tiny',
-                seeds: 1,
-                forumName: '',
-                size: 0,
-                topicId: 0,
-                topicUrl: null
-            }
-        ];
         const clusterized = clusterizeResults(searchResults, 'seeds');
-        expect(clusterized).toEqual([clusterBig, clusterSmall]);
+        expect(clusterized).toMatchInlineSnapshot(`
+            Array [
+              Array [
+                Object {
+                  "forumName": "",
+                  "seeds": 200,
+                  "size": 0,
+                  "title": "huge",
+                  "topicId": 0,
+                  "topicUrl": null,
+                },
+                Object {
+                  "forumName": "",
+                  "seeds": 170,
+                  "size": 0,
+                  "title": "big",
+                  "topicId": 0,
+                  "topicUrl": null,
+                },
+              ],
+              Array [
+                Object {
+                  "forumName": "",
+                  "seeds": 12,
+                  "size": 0,
+                  "title": "average",
+                  "topicId": 0,
+                  "topicUrl": null,
+                },
+                Object {
+                  "forumName": "",
+                  "seeds": 5,
+                  "size": 0,
+                  "title": "not big",
+                  "topicId": 0,
+                  "topicUrl": null,
+                },
+                Object {
+                  "forumName": "",
+                  "seeds": 2,
+                  "size": 0,
+                  "title": "small",
+                  "topicId": 0,
+                  "topicUrl": null,
+                },
+                Object {
+                  "forumName": "",
+                  "seeds": 1,
+                  "size": 0,
+                  "title": "tiny",
+                  "topicId": 0,
+                  "topicUrl": null,
+                },
+              ],
+            ]
+        `);
     });
 });
